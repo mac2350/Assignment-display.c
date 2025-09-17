83a42eb4d0d530fd0bdc965b471fb148b023eb35
mac2350 <mac2012005@gmail.com>
2025-09-17 15:01:35 +0300

    Add files via upload

---
+++ display.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+Name: Macevady Mac Nganga
+REGISTRATION NUMBER: CT100/G/26121/25
+DESCRIPTION: Program that that displays all unit of the semester.
+*/
+int main()
+{
+	printf("Mathematics for science\n");
+	printf("Introduction to programming and problem solving\n");
+	printf("Analogue Electronics\n");
+	printf("Computer system and organisation\n");
+	printf("Fundamentals of ICT\n");
+	printf("Introduction to computer Apllication\n");
+	printf("Essential of Accounting\n");
+	printf("Communication skills\n");
+	
+	return 0;
+}
